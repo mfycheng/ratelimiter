f140594101ac20b187078bd1cc3790dd169529af
Mike Cheng <michael.cheng@kik.com>
2013-12-25 12:24:55 -0500

    Xcode

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,8 @@
-#include <rate_limiter_interface.hpp>
+#include <iostream>
+#include "rate_limiter_interface.hpp"
 
 int main() {
-	
+    using namespace std;
+    
+    cout << "Hello, world!" << endl;
 }
