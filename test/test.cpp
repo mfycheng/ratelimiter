06dc090f51246ed26b54af06a80b48a2f2b738f8
Mike Cheng <michael.cheng@kik.com>
2013-12-26 21:02:36 -0500

    Wrote a bunch of tests. The basic rate limiter should be finished.

--- test/test.cpp
+++ test/test.cpp
@@ -1,21 +1,101 @@
-#include <iostream>
+#include <cassert>
 #include <chrono>
+#include <iostream>
+#include <cmath>
 #include "rate_limiter.hpp"
 
-int main() {
+#define GET_TIME duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+
+bool test_aquire() {
+    // Basic test: Run a bunch of aquires, and make sure
+    // it happens in the expected time (with some tolerance)
     using namespace std::chrono;
+    
     RateLimiterInterface* limiter = new RateLimiter();
+    limiter->set_rate(10);
+    long start = GET_TIME
     
-    limiter->set_rate(1.0);
-    
-	long start = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    
-    std::cout << "Starting..." << std::endl;
-    for (int i = 0; i < 10; i++) {
-        limiter->try_aquire(1, 1000);
-        std::cout << "Aquired..." << std::endl;
+    for (int i = 0; i < 20; i++) {
+        limiter->aquire();
     }
     
-    long end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    std::cout << "Finished in " << end - start << " milliseconds" << std::endl;
+    long end = GET_TIME
+    
+    double error = ((end - start) - 2000) /  2000;
+    
+    return std::abs(error) < 0.01;
+}
+bool test_aquire_multiple_permits() {
+    // Similar to the basic test, but with various sized permits
+    using namespace std::chrono;
+    
+    RateLimiterInterface* limiter = new RateLimiter();
+    limiter->set_rate(0.5);
+
+    long start = GET_TIME
+    
+    // This aquire should happen instantly, but cause the next aquire
+    // to occur ~4 seconds later
+    limiter->aquire(2);
+    
+    long first = GET_TIME
+    
+    limiter->aquire(1);
+
+    long end = GET_TIME
+    long error = ((end - start) - 4000) / 4000;
+    
+    return (first - end < 100) && std::abs(error) < 0.01;
+}
+bool test_try_aquire() {
+    using namespace std::chrono;
+    
+    RateLimiterInterface* limiter = new RateLimiter();
+    limiter->set_rate(0.5);
+    
+    long start = GET_TIME
+    
+    // This should pass, since the current request only
+    // affects the next, and since this is the first, any size can go through
+    assert(limiter->try_aquire(1));
+    
+    // Test the timeout. Since we have a 2 second window, this should fail
+    assert(!limiter->try_aquire(1));
+    
+    // Do the same as before, but with a specified window
+    assert(!limiter->try_aquire(1, 500));
+    
+    // The previous two requests should NOT affect future requests, as they
+    // should have failed. Thus, we should have 2 seconds left, minus whatever
+    // computation time was used in the first two. This should pass. If, for some
+    // reason, the other 2 DID affect the future, then a six second window would be needed,
+    // and this should fail.
+    assert(limiter->try_aquire(1, 2000));
+    
+    long end = GET_TIME
+    double error = ((end - start) - 2000) / 2000;
+    
+    return std::abs(error) < 0.01;
+}
+
+int main() {
+    using namespace std;
+    
+    cout << "Running Aquire Test...";
+    if (test_aquire())
+        cout << "Passed." << endl;
+    else
+        cout << "Failed." << endl;
+    
+    cout << "Running Multiple Aquire Test...";
+    if (test_aquire_multiple_permits())
+        cout << "Passed." << endl;
+    else
+        cout << "Failed." << endl;
+    
+    cout << "Running try_aquire test...";
+    if (test_try_aquire())
+        cout << "Passed." << endl;
+    else
+        cout << "Failed." << endl;
 }
