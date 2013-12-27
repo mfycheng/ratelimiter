037453a28d6e7c7edd27749744a2470786d35552
Mike Cheng <michael.cheng@kik.com>
2013-12-26 21:26:51 -0500

    Macro tolerance, more tests

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,7 @@
 #include "rate_limiter.hpp"
 
 #define GET_TIME duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+#define TOLERANCE 0.001 // 0.1%
 
 bool test_aquire() {
     // Basic test: Run a bunch of aquires, and make sure
@@ -23,7 +24,7 @@ bool test_aquire() {
     
     double error = ((end - start) - 2000) /  2000;
     
-    return std::abs(error) < 0.01;
+    return std::abs(error) < TOLERANCE;
 }
 bool test_aquire_multiple_permits() {
     // Similar to the basic test, but with various sized permits
@@ -45,7 +46,7 @@ bool test_aquire_multiple_permits() {
     long end = GET_TIME
     long error = ((end - start) - 4000) / 4000;
     
-    return (first - end < 100) && std::abs(error) < 0.01;
+    return (first - end < 100) && std::abs(error) < TOLERANCE;
 }
 bool test_try_aquire() {
     using namespace std::chrono;
@@ -75,11 +76,56 @@ bool test_try_aquire() {
     long end = GET_TIME
     double error = ((end - start) - 2000) / 2000;
     
-    return std::abs(error) < 0.01;
+    return std::abs(error) < TOLERANCE;
 }
+bool test_rate() {
+    using namespace std::chrono;
+    
+    RateLimiterInterface* limiter = new RateLimiter();
+    
+    // Initial test. Set rate to 1.0, measure
+    limiter->set_rate(1);
+    assert(limiter->get_rate() == 1);
+    
+    long start = GET_TIME
+    
+    limiter->aquire();
+    limiter->aquire();
+    
+    long end = GET_TIME
+    double first_error = ((end - start) - 1000) / 1000;
+    
+    limiter->set_rate(0.5);
+    assert(limiter->get_rate() == 0.5);
+    
+    // This is where it gets interesting! The previous aquire()
+    // set the wait time for the next aquire() to be 1 second, but we
+    // changed the rate right after! However, the implementation doesn't
+    // keep track of the last request, just when the new one can take place. Thus,
+    // the new rate will be applied on the next aquire.
+    
+    limiter->aquire();
+    
+    // After this aquire, we will have waited the 1 second. We expect the next
+    // one to happen in 2 seconds, instead of 1
+    start = GET_TIME
+    limiter->aquire();
+    end = GET_TIME
+    
+    long second_error = ((end - start) - 2000) / 2000;
+    
+    return std::abs(first_error) < TOLERANCE && std::abs(second_error) < TOLERANCE;
+}
+
+bool test_concurrent() {
+    return false;
+}
+
 
 int main() {
     using namespace std;
+    
+    // TODO: Make running tests more automated
     
     cout << "Running Aquire Test...";
     if (test_aquire())
@@ -95,6 +141,12 @@ int main() {
     
     cout << "Running try_aquire test...";
     if (test_try_aquire())
+        cout << "Passed." << endl;
+    else
+        cout << "Failed." << endl;
+    
+    cout << "Running rate test...";
+    if (test_rate())
         cout << "Passed." << endl;
     else
         cout << "Failed." << endl;
